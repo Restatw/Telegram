b016f0630330797a16b2173f1fc7b1e99e807d2e
Restatw <xz84121a@gmail.com>
2021-02-15 00:56:14 +0800

    Revert "fixed getChatAdministrators"
    This reverts commit 618570105ea7fd60fcc76f6fffda1114421ea481.

--- src/test.ts
+++
@@ -1,7 +0,0 @@
-import { Telegram } from './app'
-
-Telegram.Bot.api.getChat("1155177458:AAHGw1XX4VVAzK7tgiOqLXzJiu_fyk4pL78",705801802).then(
-    result => console.log(result)
-).catch(
-    error => console.log(error)
-)
